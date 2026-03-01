eaede12a60952def65c38d9fef80fcc1455fcb62
Samy Rahmani <samy.osmium@gmail.com>
2026-03-01 11:28:42 -0500

    feat: init

---
+++ lua/community.lua
@@ -0,0 +1,32 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  -- { import = "astrocommunity.media.vim-wakatime" },
+
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.astro" },
+
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.biome" },
+
+  { import = "astrocommunity.code-runner.vim-slime" },
+
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
+  -- import/override with your plugins folder
+}

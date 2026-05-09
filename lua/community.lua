bdfb1320832d7dd1424795b83912724f1ec7247e
samyosm <samy.osmium@gmail.com>
2026-05-09 14:24:12 -0400

    feat: migrated to v6

--- lua/community.lua
+++ lua/community.lua
@@ -5,29 +5,26 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-
-  -- { import = "astrocommunity.media.vim-wakatime" },
-
   { import = "astrocommunity.pack.markdown" },
 
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.json" },
 
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.astro" },
+  -- { import = "astrocommunity.pack.tailwindcss" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.html-css" },
+  -- { import = "astrocommunity.pack.astro" },
 
-  { import = "astrocommunity.pack.julia" },
-  { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.julia" },
+  -- { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.biome" },
+  -- { import = "astrocommunity.pack.biome" },
 
-  { import = "astrocommunity.code-runner.vim-slime" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+
+  -- { import = "astrocommunity.code-runner.vim-slime" },
 
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
-
-  -- import/override with your plugins folder
 }
